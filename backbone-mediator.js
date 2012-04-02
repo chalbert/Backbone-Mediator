51989b68fdb7bd84eddd05139b5446e6cda8f7b7
Nicolas Gilbert <nic.g.gilbert@gmail.com>
2012-04-02 14:43:04 +1200

    Made AMD format optinal, added shortcut functions

--- backbone-mediator.js
+++ backbone-mediator.js
@@ -1,48 +1,111 @@
-define([
-  'underscore',
-  'libs/vendor/backbone/backbone'
-], function(_, Backbone){
+/**
+ * |-------------------|
+ * | Backbone-Mediator |
+ * |-------------------|
+ *  Backbone-Mediator is freely distributable under the MIT license.
+ *
+ *  <a href="https://github.com/chalbert/Backbone-Mediator">More details & documentation</a>
+ *
+ * @author Nicolas Gilbert
+ *
+ * @requires _
+ * @requires Backbone
+ */
+(function(factory){
+  'use strict';
 
+  if (typeof define === 'function' && define.amd) {
+    define(['underscore', 'backbone'], factory);
+  } else {
+    factory(_, Backbone);
+  }
+
+})(function (_, Backbone){
+  'use strict';
+
+  /**
+   * @static
+   */
   var channels = {};
 
   Backbone.Mediator = {
 
+    /**
+     * Subscribe to a global event
+     *
+     * @param channel
+     */
     subscribe: function(channel, subscription, context, once) {
       if (!channels[channel]) channels[channel] = [];
       channels[channel].push({fn: subscription, context: context || this, once: once});
     },
 
+    /**
+     * Trigger all callbacks for a channel
+     *
+     * @param channel
+     * @params N Extra parametter to pass to handler
+     */
     publish: function(channel) {
       if (!channels[channel]) return;
-      var args = [].slice.call(arguments, 1);
+
+      var args = [].slice.call(arguments, 1),
+          subscription;
+
       for (var i = 0; i < channels[channel].length; i++) {
-        var subscription = channels[channel][i];
+        subscription = channels[channel][i];
         subscription.fn.apply(subscription.context, args);
         if (subscription.once) {
-          this.unsubscribe(channel, subscription.fn, subscription.context);
+          Backbone.Mediator.unsubscribe(channel, subscription.fn, subscription.context);
           i--;
         }
       }
     },
 
+    /**
+     * Cancel subscription
+     *
+     * @param channel
+     * @param fn
+     * @param context
+     */
+
     unsubscribe: function(channel, fn, context){
-      if (channels[channel]) {
-        for (var i = 0; i < channels[channel].length; i++) {
-          var subscription = channels[channel][i];
-          if (subscription.fn === fn && subscription.context === context) {
-            channels[channel].splice(i, 1);
-            i--;
-          }
+      if (!channels[channel]) return;
+
+      var subscription;
+      for (var i = 0; i < channels[channel].length; i++) {
+        subscription = channels[channel][i];
+        if (subscription.fn === fn && subscription.context === context) {
+          channels[channel].splice(i, 1);
+          i--;
         }
       }
     },
 
+    /**
+     * Currying for subscribing to one event only
+     *
+     * @param channel
+     * @param subscription
+     * @param context
+     */
     subscribeOnce: function (channel, subscription, context) {
       Backbone.Mediator.subscribe(channel, subscription, context, true);
     }
 
   };
 
+  /**
+   * Shortcuts
+   */
+  _.extend(Backbone.Mediator, {
+    pub: Backbone.Mediator.publish,
+    sub: Backbone.Mediator.subscribe
+  });
+
   return Backbone;
+
+
 
 });
